619308db68139024f13577129ca006f166fa5d08
axesgeeks2021 <kamalsingh87912711@gmail.com>
2023-08-04 16:45:10 +0530

    changes

--- src/pages/Team/AssignedJobs.jsx
+++ src/pages/Team/AssignedJobs.jsx
@@ -50,8 +50,7 @@ function AssignedJobs() {
                     <Navigation />
                 </div>
        
-                <div style={{ width: '100%', padding: '20px 10px' }}>
-                    {/* <Button title="Create New Customer" background="green" margin="4px 0" color="white" onclick={() => setShowForm(!showForm)} /> */}
+                {/* <div style={{ width: '100%', padding: '20px 10px' }}>
                     <ul className="responsive-table">
                         <li className="table-header">
                             <div className="col col-2 text-center text-slate-50 text-base font-bold">Project</div>
@@ -84,8 +83,47 @@ function AssignedJobs() {
                             })
                         }
                     </ul>
+                </div> */}
+                   <div class="container__table">
+            <div className='py-2 flex justify-end'>
+                    <Button title="Create New Order" background="green" color="white" onclick={() => setShowForm(!showForm)} />
                 </div>
-
+                <table class="responsive-table">
+                    {/* <caption>Top 10 Grossing Animated Films of All Time</caption> */}
+                    <thead>
+                        <tr>
+                            <th scope="col">Name</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Mobile</th>
+                            <th scope="col">City / State</th>
+                            <th scope="col">Type</th>
+                            <th scope="col">Apporved Status</th>
+                            {/* <th scope="col">Budget</th> */}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            orderList.length < 1 ? <h2>There is no order available right now...</h2> : orderList.map((ele, idx) => {
+                                return (
+                                    // <Link to="/non-admin/orders" state={{ ele }} key={idx}>
+                                        <tr>
+                                            <th scope="row">{ele?.to_address?.user?.first_name}</th>
+                                            <td data-title="Released">{ele?.to_address?.user?.email}</td>
+                                            <td data-title="Studio">{ele?.to_address?.user?.phone}</td>
+                                            {/* <td data-title="Worldwide Gross" data-type="currency">{ele.building_Type}</td>
+                                            <td data-title="Domestic Gross" data-type="currency">{ele.nmi_no}</td>
+                                            <td data-title="International Gross" data-type="currency">
+                                            <Button title="In Process" background="green" color="white"  />
+                                            </td> */}
+                                            {/* <td data-title="Budget" data-type="currency">$260,000,000</td> */}
+                                        </tr>
+                                    // </Link>
+                                )
+                            })
+                        }
+                    </tbody>
+                </table>
+            </div>
             </div>
         </>
     )
--- src/pages/nonAdmin/organization/Customer.jsx
+++ src/pages/nonAdmin/organization/Customer.jsx
@@ -170,7 +170,7 @@ function Customer() {
                 <div>
                     <NonAdminSideNavigation />
                 </div>
-                <div style={{ width: '100%', padding: '20px 10px' }}>
+                {/* <div style={{ width: '100%', padding: '20px 10px' }}>
                     <Button title="Create New Customer" background="green" margin="4px 0" color="white" onclick={() => setShowForm(!showForm)} />
                     <ul className="responsive-table">
                         <li className="table-header">
@@ -182,21 +182,61 @@ function Customer() {
                             <div className="col col-2 text-center text-slate-50 text-base font-bold">Apporved Status</div>
                         </li>
                         {
-                            customerList?.map((ele, idx) => {
+                            customerList?.map((ele, idx) => { 
                                 return (
                                     <li className="table-row" key={idx}>
                                         <div className={`col col-2 text-center`}>{ele?.to_address?.user?.first_name}</div>
                                         <div className={`col col-2 text-center`}>{ele?.to_address?.user?.email}</div>
                                         <div className={`col col-2 text-center`}>{ele?.to_address?.user?.phone}</div>
-                                        {/* <div className={`col col-2 text-center`}>{ele.admin.city} / {ele.admin.state}</div> */}
-                                        {/* <div className={`col col-2 text-center`}>{ele.admin.user.user_type}</div>
-                                        <div className={`col col-2 text-center`}>{ele.admin.user.has_approve === false ? 'Not Approved' : 'Approved'}</div> */}
+                                        <div className={`col col-2 text-center`}>{ele.admin.city} / {ele.admin.state}</div>
+                                        <div className={`col col-2 text-center`}>{ele.admin.user.user_type}</div>
+                                        <div className={`col col-2 text-center`}>{ele.admin.user.has_approve === false ? 'Not Approved' : 'Approved'}</div>
                                     </li>
                                 )
                             })
                         }
                     </ul>
+                </div> */}
+                <div class="container__table">
+            <div className='py-2 flex justify-end'>
+                    <Button title="Create New Order" background="green" color="white" onclick={() => setShowForm(!showForm)} />
                 </div>
+                <table class="responsive-table">
+                    {/* <caption>Top 10 Grossing Animated Films of All Time</caption> */}
+                    <thead>
+                        <tr>
+                            <th scope="col">Name</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Mobile</th>
+                            <th scope="col">City / State</th>
+                            <th scope="col">Type</th>
+                            <th scope="col">Apporved Status</th>
+                            {/* <th scope="col">Budget</th> */}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            customerList.length < 1 ? <h2>There is no order available right now...</h2> : customerList.map((ele, idx) => {
+                                return (
+                                    // <Link to="/non-admin/orders" state={{ ele }} key={idx}>
+                                        <tr>
+                                            <th scope="row">{ele?.to_address?.user?.first_name}</th>
+                                            <td data-title="Released">{ele?.to_address?.user?.email}</td>
+                                            <td data-title="Studio">{ele?.to_address?.user?.phone}</td>
+                                            {/* <td data-title="Worldwide Gross" data-type="currency">{ele.building_Type}</td>
+                                            <td data-title="Domestic Gross" data-type="currency">{ele.nmi_no}</td>
+                                            <td data-title="International Gross" data-type="currency">
+                                            <Button title="In Process" background="green" color="white"  />
+                                            </td> */}
+                                            {/* <td data-title="Budget" data-type="currency">$260,000,000</td> */}
+                                        </tr>
+                                    // </Link>
+                                )
+                            })
+                        }
+                    </tbody>
+                </table>
+            </div>
                 {
                     showForm &&
                     <div style={{ width: "100%", display: 'flex', position: 'absolute', background: 'white', justifyContent: "center", alignItems: 'center', flexDirection: 'column' }}>
